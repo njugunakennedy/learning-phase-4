19c5d94d1a2255bd6981f154f2cc0d91c728cd12
kennedy njuguna <njugunkennedy5@gmail.com>
2023-11-25 15:24:18 +0900

    changes

--- src/app/api/pets/route.ts
+++ src/app/api/pets/route.ts
@@ -1,9 +1,13 @@
 import { NextResponse } from 'next/server'
+import { type NextRequest } from 'next/server'
 
 import prisma from '../../../../lib/prisma'
 
 // GET /api/pets
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const queryName = searchParams.get('name')
+
   const pets = await prisma.pet.findMany({
     // sort by id ascending
     orderBy: { id: 'asc' },
@@ -11,6 +15,7 @@ export async function GET() {
     // include: { owner: true },
     // select only id, name, imageUrl, and owner.name
     select: { id: true, name: true, imageUrl: true, owner: { select: { name: true } } },
+    where: queryName ? { name: { contains: queryName } } : undefined,
   })
   // return Response with pets to json
   return NextResponse.json({ pets })
