1d77cd83677d01e1409d81df25610adac958cbbd
kennedy njuguna <njugunkennedy5@gmail.com>
2023-11-25 11:33:30 +0900

    fix error

--- src/app/api/pets/route.ts
+++ src/app/api/pets/route.ts
@@ -43,17 +43,3 @@ export async function PUT(request: Request) {
   // return Response with pet to json
   return NextResponse.json({ pets })
 }
-// GET /api/pets/:id
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-  // get id from params
-  const id = params.id
-  // findUnique returns a single pet with owner data
-  const pet = await prisma.pet.findUnique({
-    // where id is equal to the id param
-    where: { id: Number(id) },
-    // include owner data
-    include: { owner: true, weights: true },
-  })
-  // return Response with pets to json
-  return NextResponse.json({ pet })
-}
